91d97d81ca4010bd59d63f3540bc849a7258696c
Victor Makoed <vmakoed@gmail.com>
2019-01-24 00:48:06 +0300

    much code

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'rubocop', '~> 0.63.1'
 gem 'telegram_bot', '~> 0.0.8'
 gem 'weighted_randomizer', '~> 0.1.2'
--- bot.rb
+++ bot.rb
@@ -1,18 +1,39 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'telegram_bot'
 require 'weighted_randomizer'
 
-TOKEN = '626334553:AAG_TKzC6iwbgihU2hgQZlZT9VSG0hqxs8M'
+TOKEN = ENV['SMYSLOBOT_TOKEN']
+SYMBOLS_THRESHOLD = ENV['SMYSLOBOT_THRESHOLD'].to_i
 REPLY = 'Ух, сука, со смыслом'
-FALSE_DECISION_FREQUENCY_RATE = 20
-DECISIONS = { true => 1, false => FALSE_DECISION_FREQUENCY_RATE }
+FIBONACCI = [
+  3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 610, 987, 1597, 584, 4181, 6765
+].freeze
+
+@big_messages_total = 0
+@fibonacci_counter = 0
+
+find_smysl = lambda do
+  smysl = (@big_messages_total % FIBONACCI[@fibonacci_counter]).zero?
+  smysl.tap { |any_smysl| @big_messages_total = 0 if any_smysl }
+end
+
+has_smysl = lambda do |text|
+  return false if text.size < SYMBOLS_THRESHOLD
+
+  @big_messages_total += 1
+  find_smysl.call
+end
+
+so_smyslom = lambda do |reply, bot|
+  reply.tap { |reply_object| reply_object.text = REPLY }.send_with(bot)
+  @fibonacci_counter += 1
+end
 
 bot = TelegramBot.new(token: TOKEN)
-randomizer = WeightedRandomizer.new(DECISIONS)
-
 bot.get_updates(fail_silently: true) do |message|
-  puts "@#{message.from.username}: #{message.text}"
-  message.reply do |reply|
-    randomizer.sample && reply.tap { |reply_object| reply_object.text = REPLY }.send_with(bot)
-  end
+  next unless has_smysl.call(message.text)
+
+  message.reply { |reply| so_smyslom.call(reply, bot) }
 end
